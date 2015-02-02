858c1be6beb39c94790fe0ab7207640a6af8f237
Shane Burkhart <shaneburkhart@gmail.com>
2015-02-02 23:12:44 +0000

    Added unique constraint to users table.

--- db/migrations/20150114025706_add_user_table.go
+++ db/migrations/20150114025706_add_user_table.go
@@ -9,7 +9,7 @@ func Up_20150114025706(txn *sql.Tx) {
 	txn.Exec(`
 		CREATE TABLE users (
 		id SERIAL NOT NULL PRIMARY KEY,
-		email varchar(255),
+		email varchar(255) UNIQUE,
 		password_digest varchar(60)
 		)
 	`)
